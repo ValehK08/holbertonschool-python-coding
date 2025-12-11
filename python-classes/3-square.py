06eea44e1dd75017510c0f7bbff7581a5f879ff2
ValehK08 <valeh.kazimli@outlook.com>
2025-12-11 14:26:50 +0000

    .

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -8,6 +8,10 @@ class Square:
     def __init__(self, size=0):
         """INIT SIIIZE"""
 
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >=0")
         self.size(size)
     
     @property
@@ -27,4 +31,5 @@ class Square:
         self.__size = siz
 
     def area(self):
+        """AREAAAAA"""
         return self.__size**2
